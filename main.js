7408f16821b08a34aa7cdd3009d47494f8146698
mybearworld <130385691+mybearworld@users.noreply.github.com>
2025-04-11 19:32:18 +0200

    Allow checking for a word to be written

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 // @ts-check
 (async () => {
+  // from https://en.wikipedia.org/w/index.php?title=Morse_code&diff=prev&oldid=1283847196
+  // prettier-ignore
+  /** @type {Record<string, string[]>} */
+  const morseCodeMappings = {'.-': ['a'], '-...': ['b'], '-.-.': ['c'], '-..': ['d'], '.': ['e'], '..-.': ['f'], '--.': ['g'], '....': ['h'], '..': ['i'], '.---': ['j'], '.-..': ['l'], '--': ['m'], '-.': ['n'], '---': ['o'], '.--.': ['p'], '--.-': ['q'], '.-.': ['r'], '...': ['s'], '-': ['t'], '..-': ['u'], '...-': ['v'], '.--': ['w'], '-..-': ['x'], '-.--': ['y'], '--..': ['z'], '-----': ['0'], '.----': ['1'], '..---': ['2'], '...--': ['3'], '....-': ['4'], '.....': ['5'], '-....': ['6'], '--...': ['7'], '---..': ['8'], '----.': ['9'], '.-.-.-': ['.'], '--..--': [','], '..--..': ['?'], '.----.': ["'"], '-.-.--': ['!'], '-..-.': ['/'], '-.--.-': [')'], '---...': [':'], '-.-.-.': [';'], '-....-': ['-'], '..--.-': ['_'], '.-..-.': ['"'], '...-..-': ['$'], '.--.-.': ['@'], '.--.-': ['à', 'å'], '.-.-': ['ä', 'ą', 'æ'], '-.-..': ['ć', 'ĉ', 'ç'], '----': ['ch', 'ĥ', 'š'], '..-..': ['ð', 'é', 'ę'], '..--.': ['đ'], '.-..-': ['è', 'ł'], '--.-.': ['ĝ'], '.---.': ['ĵ'], '--.--': ['ń', 'ñ'], '---.': ['ó', 'ö', 'ø'], '...-...': ['ś'], '.--..': ['þ'], '..--': ['ü', 'ŭ'], '--..-.': ['ź'], '--..-': ['ż']};
+  const checkMorseCode = (
+    /** @type {string[]} */ morse,
+    /** @type {string} */ word
+  ) => {
+    for (const letter of morse) {
+      if (!(letter in morseCodeMappings)) return false;
+      const meaning = morseCodeMappings[letter].find((meaning) =>
+        word.startsWith(meaning)
+      );
+      if (!meaning) return false;
+      word = word.slice(meaning.length);
+    }
+    return true;
+  };
+
   const getWord = () => {
     return new Promise((resolve) => {
       /** @type {string[]} */
@@ -36,6 +55,7 @@
   };
 
   while (true) {
-    console.log(await getWord());
+    const word = await getWord();
+    console.log(checkMorseCode(word, "sos"), checkMorseCode(word, "ä"), word);
   }
 })();
